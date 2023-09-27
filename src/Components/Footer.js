1ed1f0a2a9f6892410d922d4c6f500450598a2f8
aamirsk8766 <123264209+aamirsk8766@users.noreply.github.com>
2023-09-27 18:44:10 +0530

    ,

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,5 @@
 import React from "react";
 import Logo from "../Assets/company_logo.png";
-import { BsTwitter } from "react-icons/bs";
-import { SiLinkedin } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -12,31 +8,20 @@ const Footer = () => {
         <div className="footer-logo-container">
           <img src={Logo} alt="" />
         </div>
-        <div className="footer-icons">
-          <BsTwitter />
-          <SiLinkedin />
-          <BsYoutube />
-          <FaFacebookF />
-        </div>
+       
       </div>
       <div className="footer-section-two">
         <div className="footer-section-columns">
-          <span>Qualtiy</span>
-          <span>Help</span>
-          <span>Share</span>
-          <span>Carrers</span>
-          <span>Testimonials</span>
-          <span>Work</span>
+          <span>Phone Number</span>
+          <span>Email</span>
+         
         </div>
         <div className="footer-section-columns">
-          <span>244-5333-7783</span>
-          <span>hello@food.com</span>
-          <span>press@food.com</span>
-          <span>contact@food.com</span>
+          <span>9060602460</span>
+          <span>mrappleindia@gmail.com</span><br />
+          
         </div>
         <div className="footer-section-columns">
-          <span>Terms & Conditions</span>
-          <span>Privacy Policy</span>
         </div>
       </div>
     </div>
